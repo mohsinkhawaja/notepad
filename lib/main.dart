fd4701e197e5ddac1a8052318c3a4e49b2ab8e59
Mohsin Ali Rashid <mohsinalikhawaja1111@gmail.com>
2024-02-06 22:05:42 -0800

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:notepad/screens/login_screen.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
